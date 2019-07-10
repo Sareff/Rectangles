31ea7a2a6ffe55d2ae6849b332dbeb29164cd11c
Sareff <ilbezr@gmail.com>
2019-07-10 20:57:55 +0300

    Initial Commit

---
+++ Rectangles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rectangles
+//
+//  Created by Admin on 10/07/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
